b8bd14b100df7edfd7075565890602b8b2fe17bb
Jared Crowe <jcrowe@atlassian.com>
2018-01-22 09:11:46 +1100

    Implement ViewTracker utility component and render it within PageBoundary component

--- src/modules/analytics/PageBoundary.js
+++ src/modules/analytics/PageBoundary.js
@@ -1,12 +1,19 @@
 // @flow
 
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 import withAnalytics from './withAnalytics';
+import ViewTracker from './ViewTracker';
 
 class PageBoundary extends Component {
   render() {
-    return this.props.children;
+    const { analyticsNamespace, children } = this.props;
+    return (
+      <div>
+        <ViewTracker page={analyticsNamespace} />
+        {children}
+      </div>
+    );
   }
 }
 
