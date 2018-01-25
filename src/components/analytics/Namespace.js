96226cc50ba8eeaea2e79540842308c37192ebbf
Michael Blaszczyk <mblaszczyk@atlassian.com>
2018-01-25 16:55:05 +1100

    Fix bug in page boundary where it was causing Namespace to return multiple elements

--- src/components/analytics/Namespace.js
+++ src/components/analytics/Namespace.js
@@ -1,14 +1,14 @@
 // @flow
 
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 import { withAnalytics } from '../../modules/analytics';
 
 /** A convenience component which allows you to drop a new analyticsNamespace
  * layer in the page without having to wrap anything in withAnalytics. */
-class Namespace extends Component {
+class Namespace extends Component<*> {
   render() {
-    return this.props.children;
+    return React.Children.only(this.props.children);
   }
 }
 
--- src/components/analytics/PageBoundary.js
+++ src/components/analytics/PageBoundary.js
@@ -7,13 +7,15 @@ import ViewTracker from './ViewTracker';
 
 /** A convenience component which combines a Namespace and a ViewTracker. Useful
  * for declaring 'page boundaries'. */
-export default class PageBoundary extends Component {
+export default class PageBoundary extends Component<*> {
   render() {
     const { analyticsNamespace, children } = this.props;
     return (
       <Namespace analyticsNamespace={analyticsNamespace}>
-        <ViewTracker page={analyticsNamespace} />
-        {children}
+        <div>
+          <ViewTracker page={analyticsNamespace} />
+          {children}
+        </div>
       </Namespace>
     );
   }
