8b806836826901070748989a494cb116e806bcf8
Michael Blaszczyk <mblaszczyk@atlassian.com>
2018-02-05 12:16:00 +1100

    Add test stubs for analytics module

---
+++ src/modules/analytics/__tests__/AnalyticsEvent.js
@@ -0,0 +1,19 @@
+// @flow
+
+import AnalyticsEvent from '../AnalyticsEvent';
+
+xit('can be created with action and payload args', () => {
+
+});
+
+xit('can be cloned into a new event', () => {
+
+});
+
+xit('payload can be updated with an object arg', () => {
+
+});
+
+xit('payload can be updated with a function arg', () => {
+
+});
---
+++ src/modules/analytics/__tests__/UIAnalyticsEvent.js
@@ -0,0 +1,13 @@
+// @flow
+
+import UIAnalyticsEvent from '../UIAnalyticsEvent';
+
+xit('should construct with relevant args', () => {});
+
+xit('can be cloned into a new event', () => {});
+
+xit('can be updated', () => {});
+
+xit('executes all event handlers when fired', () => {});
+
+xit('cannot be interacted with via method calls after being fired', () => {});
