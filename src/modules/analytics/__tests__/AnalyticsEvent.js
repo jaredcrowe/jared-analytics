a17bc9b7028e9b502fd4b7c467f921584ba86f79
Michael Blaszczyk <mblaszczyk@atlassian.com>
2018-02-06 12:31:13 +1100

    Add tests for AnalyticsEvent

--- src/modules/analytics/__tests__/AnalyticsEvent.js
+++ src/modules/analytics/__tests__/AnalyticsEvent.js
@@ -2,18 +2,68 @@
 
 import AnalyticsEvent from '../AnalyticsEvent';
 
-xit('can be created with action and payload args', () => {
-
+it('should be constructed with both action and payload args', () => {
+  const analyticsEvent = new AnalyticsEvent({ action: 'click', payload: { a: 'b' } });
+  expect(analyticsEvent).toEqual(expect.any(AnalyticsEvent));
+  expect(analyticsEvent.action).toBe('click');
+  expect(analyticsEvent.payload).toEqual({ a: 'b' });
 });
 
-xit('can be cloned into a new event', () => {
-
+it('can be cloned into a new event which has the same action and payload', () => {
+  const analyticsEvent = new AnalyticsEvent({ action: 'click', payload: {
+    a: { b: 'c' },
+  }});
+  const clonedEvent = analyticsEvent.clone();
+  expect(analyticsEvent.action).toEqual(clonedEvent.action);
+  expect(analyticsEvent.payload).toEqual(clonedEvent.payload);
 });
 
-xit('payload can be updated with an object arg', () => {
+it('should deep clone event payloads when cloning', () => {
+  const analyticsEvent = new AnalyticsEvent({ action: 'click', payload: {
+    a: { b: 'c' },
+  }});
+  const clonedEvent = analyticsEvent.clone();
+  expect(analyticsEvent).not.toBe(clonedEvent);
+  expect(analyticsEvent.payload).not.toBe(clonedEvent.payload);
+  expect(analyticsEvent.payload.a).not.toBe(clonedEvent.payload.a);
+})
 
+it('payload can be updated with an object that is shallow merged', () => {
+  const analyticsEvent = new AnalyticsEvent({
+    action: 'click', payload: {
+      a: { b: 'c' },
+    }
+  });
+  analyticsEvent.update({ d: 'e' });
+  expect(analyticsEvent.payload).toEqual({
+    a: { b: 'c' },
+    d: 'e',
+  });
+
+  analyticsEvent.update({ a: { f: 'g' } });
+  expect(analyticsEvent.payload).toEqual({
+    a: { f: 'g' },
+    d: 'e',
+  });
 });
 
-xit('payload can be updated with a function arg', () => {
+it('payload can be updated with a function', () => {
+  const analyticsEvent = new AnalyticsEvent({
+    action: 'click', payload: {
+      a: { b: 'c' },
+      d: 'e',
+    }
+  });
 
+  analyticsEvent.update( payload => ({
+    ...payload,
+    a: {
+      ...payload.a,
+      f: 'g',
+    }
+  }));
+  expect(analyticsEvent.payload).toEqual({
+    a: { b: 'c', f: 'g' },
+    d: 'e',
+  });
 });
